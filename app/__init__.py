e048ef4f29bca150ac2f5ef5416d32632b19a8bc
harry <harrisongitau3@gmail.com>
2021-04-20 04:21:45 +0300

    Handle errors

--- app/__init__.py
+++ app/__init__.py
@@ -7,6 +7,9 @@ from flask_mail import Mail
 from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
 from flask_moment import Moment
+import logging
+from logging.handlers import SMTPHandler, RotatingFileHandler
+import os
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -20,4 +23,43 @@ login.login_view = 'login'
 bcrypt = Bcrypt(app)
 moment = Moment(app)
 
-from app import routes, models
+if not app.debug:
+    if app.config['MAIL_SERVER']:
+        auth = None
+        if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
+            auth = (app.config['MAIL_USERNAME'],
+                    app.config['MAIL_PASSWORD']
+                    )
+        secure = None
+        if app.config['MAIL_USE_TLS']:
+            secure = ()
+        mail_handler = SMTPHandler(
+            mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
+            fromaddr='noreply@' + app.config['MAIL_SERVER'],
+            toaddrs=app.config['ADMINS'],
+            subject='Comment Moderation Failure',
+            credentials=auth, secure=secure
+        )
+        mail_handler.setLevel(logging.ERROR)
+        app.logger.addHandler(mail_handler)
+    if app.config['LOG_TO_STDOUT']:
+        stream_handler = logging.StreamHandler()
+        stream_handler.setLevel(logging.INFO)
+        app.logger.addHandler(stream_handler)
+    else:
+        if not os.path.exists('logs'):
+            os.mkdir('logs')
+        file_handler = RotatingFileHandler(
+            'logs/comment_moderation.log',
+            maxBytes=10240,
+            backupCount=10
+        )
+        file_handler.setFormatter(logging.Formatter(
+            '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'
+        ))
+        file_handler.setLevel(logging.INFO)
+        app.logger.addHandler(file_handler)
+        app.logger.setLevel(logging.INFO)
+        app.logger.info('Testing Comment Moderation')
+
+from app import routes, models, errors
