d876f83a8c619d9a484890eb7a1b1a11b8bd739d
harry <harrisongitau3@gmail.com>
2021-05-01 08:08:51 +0300

    Fix: Send password reset token

--- app/email.py
+++ app/email.py
@@ -21,13 +21,18 @@ def send_mail(subject, sender, recipients, text_body, html_body):
 # ---------------------
 
 
-def send_password_reset_email(user):
+def send_password_reset_email(admin):
+    token = admin.get_reset_password_token()
     send_mail(
         '[Password Reset]',
         sender=app.config['ADMINS'][0],
-        recipients=[user.email],
-        text_body=render_template('email/reset_password.txt', user=user),
-        html_body=render_template('email/reset_password.html', user=user)
+        recipients=[admin.email],
+        text_body=render_template('email/reset_password.txt',
+                                  admin=admin,
+                                  token=token),
+        html_body=render_template('email/reset_password.html',
+                                  admin=admin,
+                                  token=token)
     )
 
 
