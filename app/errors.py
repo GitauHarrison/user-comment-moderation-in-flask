52bb720b941da8add88bd7eab4c4e4786efcf519
harry <harrisongitau3@gmail.com>
2021-12-05 04:05:37 +0300

    Dev: Add a 500 error template

--- app/errors.py
+++ app/errors.py
@@ -1,5 +1,11 @@
-from app import app
 from flask import render_template
+from app import app, db
+
+
+@app.errorhandler(500)
+def internal_error(error):
+    db.session.rollback()
+    return render_template('500.html'), 500
 
 
 @app.errorhandler(404)
