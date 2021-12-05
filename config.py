8dc4afaf87dd941d1280fc30f1c54f92ebd732d9
harry <harrisongitau3@gmail.com>
2021-12-05 12:50:26 +0300

    Dev: Add heroku configuration variables

--- config.py
+++ config.py
@@ -7,6 +7,10 @@ class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
 
     # Database
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(os.path.dirname(__file__), 'app.db')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', '').replace(
+        'postgres://', 'postgresql://') or\
+        'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    # Heroku
+    LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
